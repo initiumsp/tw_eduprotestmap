7eb2e4b140f68a58d05f6aaa28be2158ce100325
Andy Shu Xin <andyshuxin@gmail.com>
2015-08-03 18:37:32 +0800

    change stack to svg

---
+++ fedData_alt.js
@@ -0,0 +1,26 @@
+var protestData = [
+  {counties: ['path2531', 'path3699', 'path3635'],
+   descriptions: []
+  },
+  {counties: ['path2707', 'path2726', 'path2732', 'path2781'],
+    descriptions: []
+  },
+  {counties: ['path2792', 'path2425', 'path2785', 'path2812'],
+    descriptions: []
+  },
+  {counties: ['path2907', 'path4308'],
+    descriptions: []
+  },
+  {counties: ['path3690', 'path2843', 'path2856'],
+    descriptions: []
+  },
+  {counties: ['path2905'],
+    descriptions: []
+  },
+  {counties: ['path3082'],
+    descriptions: []
+  },
+  {counties: ['path2861'],
+    descriptions: []
+  },
+];
